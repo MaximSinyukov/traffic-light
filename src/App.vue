5459ba12d240d60342eac5cac6a194ada82b7457
Maxim Sinyukov <verero1173@yandex.ru>
2021-04-10 21:34:15 +0300

    feat: сверстать контейнер светофора и подключить к приложению

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <template>
   <div id="app">
+    <h1>Traffic light</h1>
+    <TrafficLight />
   </div>
 </template>
+
+<script>
+import TrafficLight from '@/components/TrafficLight'
+
+export default {
+  name: 'app',
+  components: {
+    TrafficLight
+  }
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: calc(100vh - 16px);
+  min-height: 530px;
+  justify-content: center;
 }
 </style>
---
+++ src/components/TrafficLight.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="traffic-light"></div>
+</template>
+
+<style>
+.traffic-light {
+	background:#333;
+	width: 150px;
+	height: 440px;
+	border-radius: 10px;
+	font-size: 70px;
+	text-align: center;
+	padding: 1px 0;
+	border: 6px solid #000;
+	margin: auto;
+	margin: 0 auto;
+}
+</style>
